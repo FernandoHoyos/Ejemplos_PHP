ff3da585cb4bb2860a842c6ef72c4191c3fa47e5
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-08 19:47:01 +0200

    Segundo ejemplo

---
+++ 02_html_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h2></h2>
+    <p>Este párrafo tiene contenido estático HTML</p>
+    <p><?php echo "<b>Este párrafo es dinámico tiene PHP</b>"; ?></p>
+</body>
+</html>
