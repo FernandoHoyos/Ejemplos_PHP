fc8e3a69cd5f8679b4bae6ac86556e6300ba6cba
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-17 21:22:10 +0200

    Motrar Datos 2

---
+++ 10_mostrarDatos.php
@@ -0,0 +1,18 @@
+<?php 
+    //recuperar la opción seleccionada
+    $opcion = $_GET['opciones'];
+
+    //mostarla por pantalla
+    echo $opcion;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mostrar Datos PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
