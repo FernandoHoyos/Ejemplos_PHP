3c4d77e3255f05c4aed6e4ad4e698afa1e35657b
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-27 19:42:22 +0200

    Estructura de repetición for

---
+++ 18_for.php
@@ -0,0 +1,10 @@
+<?php 
+    //bucle for
+    //contador; condición de salida; incremento
+    for ($cont = 1; $cont <= 10; $cont++) {
+        echo "$cont <br>";
+    }
+
+    echo "valor final: $cont <p>Este valor que sobra es el ultimo intento de comprobacion del bucle for a la onceava vez</p>" ;
+
+?>
