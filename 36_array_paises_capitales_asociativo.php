bf79f0da812a1f2121d9c6a767a8c173f1801b31
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-16 16:17:52 +0100

    Associative array with the country-capital relationship / Array asociativo con la relación país-capital.

---
+++ 36_array_paises_capitales_asociativo.php
@@ -0,0 +1,15 @@
+<?php
+//crear un array asociativo con la relacion pais-capital de 5 estados.
+
+$paises['Francia'] = 'Paris';
+$paises['Ecuador'] = 'Quito';
+$paises['Italia'] = 'Roma';
+$paises['Alemania'] = 'Berlin';
+$paises['Argentina'] = 'Buenos Aires';
+$paises['Colombia'] = 'Bogota';
+
+echo '<pre>';
+print_r($paises);
+echo '</pre>';
+
+?>
